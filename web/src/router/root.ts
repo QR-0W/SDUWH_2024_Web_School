5265ea0ac36e978a0c8390c86c3a47440f004ee0
QR0W_0 <Neptune_X@foxmail.com>
2024-07-14 09:34:35 +0800

    修复BUG

--- web/src/router/root.ts
+++ web/src/router/root.ts
@@ -2,197 +2,249 @@
 const constantRouterMap = [
   // ************* 前台路由 **************
   {
-    path: '/',
-    redirect: '/index',
+    path: "/",
+    redirect: "/index"
   },
   {
-    path: '/index',
-    name: 'index',
-    redirect: '/index/portal',
-    component: () => import('/@/views/index/index.vue'),
+    path: "/index",
+    name: "index",
+    redirect: "/index/portal",
+    component: () => import("/@/views/index/index.vue"),
     children: [
       {
-        path: 'home',
-        name: 'home',
-        component: () => import('/@/views/index/home.vue'),
+        path: "home",
+        name: "home",
+        component: () => import("/@/views/index/home.vue")
       },
       {
-        path: 'login',
-        name: 'login',
-        component: () => import('/@/views/index/login.vue'),
+        path: "login",
+        name: "login",
+        component: () => import("/@/views/index/login.vue")
       },
       {
-        path: 'register',
-        name: 'register',
-        component: () => import('/@/views/index/register.vue'),
+        path: "register",
+        name: "register",
+        component: () => import("/@/views/index/register.vue")
       },
       {
-        path: 'portal',
-        name: 'portal',
-        component: () => import('/@/views/index/portal.vue'),
+        path: "portal",
+        name: "portal",
+        component: () => import("/@/views/index/portal.vue")
       },
       {
-        path: 'detail',
-        name: 'detail',
-        component: () => import('/@/views/index/detail.vue'),
+        path: "detail",
+        name: "detail",
+        component: () => import("/@/views/index/detail.vue")
       },
       {
-        path: 'confirm',
-        name: 'confirm',
-        component: () => import('/@/views/index/confirm.vue'),
+        path: "confirm",
+        name: "confirm",
+        component: () => import("/@/views/index/confirm.vue")
       },
       {
-        path: 'pay',
-        name: 'pay',
-        component: () => import('/@/views/index/pay.vue'),
+        path: "pay",
+        name: "pay",
+        component: () => import("/@/views/index/pay.vue")
       },
       {
-        path: 'jiajiaoRegister',
-        name: 'jiajiaoRegister',
-        component: () => import('/@/views/index/jiajiaoRegister.vue'),
+        path: "jiajiaoRegister",
+        name: "jiajiaoRegister",
+        component: () => import("/@/views/index/jiajiaoRegister.vue")
       },
       {
-        path: 'jiajiaocenter',
-        name: 'jiajiaocenter',
-        component: () => import('/@/views/index/jiajiaocenter.vue'),
+        path: "jiajiaocenter",
+        name: "jiajiaocenter",
+        component: () => import("/@/views/index/jiajiaocenter.vue"),
         children: [
           {
-            path: 'jiajiaoEditView',
-            name: 'jiajiaoEditView',
-            component: () => import('/@/views/index/user/jiajiao-edit-view.vue'),
+            path: "jiajiaoEditView",
+            name: "jiajiaoEditView",
+            component: () => import("/@/views/index/user/jiajiao-edit-view.vue")
           },
           {
-            path: 'jiajiaoOrderView',
-            name: 'jiajiaoOrderView',
-            component: () => import('/@/views/index/user/jiajiao-order-view.vue'),
+            path: "jiajiaoOrderView",
+            name: "jiajiaoOrderView",
+            component: () => import("/@/views/index/user/jiajiao-order-view.vue")
           },
           {
-            path: 'userInfoEditView',
-            name: 'userInfoEditView',
-            component: () => import('/@/views/index/user/userinfo-edit-view.vue'),
+            path: "userInfoEditView",
+            name: "userInfoEditView",
+            component: () => import("/@/views/index/user/userinfo-edit-view.vue")
           },
           {
-            path: 'followView',
-            name: 'followView',
-            component: () => import('/@/views/index/user/follow-view.vue'),
+            path: "followView",
+            name: "followView",
+            component: () => import("/@/views/index/user/follow-view.vue")
           },
           {
-            path: 'commentView',
-            name: 'commentView',
-            component: () => import('/@/views/index/user/comment-view.vue'),
+            path: "commentView",
+            name: "commentView",
+            component: () => import("/@/views/index/user/comment-view.vue")
           },
           {
-            path: 'securityView',
-            name: 'securityView',
-            component: () => import('/@/views/index/user/security-view.vue'),
+            path: "securityView",
+            name: "securityView",
+            component: () => import("/@/views/index/user/security-view.vue")
           },
           {
-            path: 'pushView',
-            name: 'pushView',
-            component: () => import('/@/views/index/user/push-view.vue'),
+            path: "pushView",
+            name: "pushView",
+            component: () => import("/@/views/index/user/push-view.vue")
           },
           {
-            path: 'messageView',
-            name: 'messageView',
-            component: () => import('/@/views/index/user/message-view.vue'),
-          },
-        ],
+            path: "messageView",
+            name: "messageView",
+            component: () => import("/@/views/index/user/message-view.vue")
+          }
+        ]
       },
       {
-        path: 'search',
-        name: 'search',
-        component: () => import('/@/views/index/search.vue'),
+        path: "search",
+        name: "search",
+        component: () => import("/@/views/index/search.vue")
       },
       {
-        path: 'usercenter',
-        name: 'usercenter',
-        component: () => import('/@/views/index/usercenter.vue'),
+        path: "usercenter",
+        name: "usercenter",
+        component: () => import("/@/views/index/usercenter.vue"),
         children: [
           {
-            path: 'wishThingView',
-            name: 'wishThingView',
-            component: () => import('/@/views/index/user/wish-thing-view.vue'),
+            path: "wishThingView",
+            name: "wishThingView",
+            component: () => import("/@/views/index/user/wish-thing-view.vue")
           },
           {
-            path: 'collectThingView',
-            name: 'collectThingView',
-            component: () => import('/@/views/index/user/collect-thing-view.vue'),
+            path: "collectThingView",
+            name: "collectThingView",
+            component: () => import("/@/views/index/user/collect-thing-view.vue")
           },
           {
-            path: 'userOrderView',
-            name: 'userOrderView',
-            component: () => import('/@/views/index/user/user-order-view.vue'),
+            path: "userOrderView",
+            name: "userOrderView",
+            component: () => import("/@/views/index/user/user-order-view.vue")
           },
           {
-            path: 'userInfoEditView',
-            name: 'userInfoEditView',
-            component: () => import('/@/views/index/user/userinfo-edit-view.vue'),
+            path: "userInfoEditView",
+            name: "userInfoEditView",
+            component: () => import("/@/views/index/user/userinfo-edit-view.vue")
           },
           {
-            path: 'followView',
-            name: 'followView',
-            component: () => import('/@/views/index/user/follow-view.vue'),
+            path: "followView",
+            name: "followView",
+            component: () => import("/@/views/index/user/follow-view.vue")
           },
           {
-            path: 'fansView',
-            name: 'fansView',
-            component: () => import('/@/views/index/user/fans-view.vue'),
+            path: "fansView",
+            name: "fansView",
+            component: () => import("/@/views/index/user/fans-view.vue")
           },
           {
-            path: 'scoreView',
-            name: 'scoreView',
-            component: () => import('/@/views/index/user/score-view.vue'),
+            path: "scoreView",
+            name: "scoreView",
+            component: () => import("/@/views/index/user/score-view.vue")
           },
           {
-            path: 'commentView',
-            name: 'commentView',
-            component: () => import('/@/views/index/user/comment-view.vue'),
+            path: "commentView",
+            name: "commentView",
+            component: () => import("/@/views/index/user/comment-view.vue")
           },
           {
-            path: 'securityView',
-            name: 'securityView',
-            component: () => import('/@/views/index/user/security-view.vue'),
+            path: "securityView",
+            name: "securityView",
+            component: () => import("/@/views/index/user/security-view.vue")
           },
           {
-            path: 'pushView',
-            name: 'pushView',
-            component: () => import('/@/views/index/user/push-view.vue'),
+            path: "pushView",
+            name: "pushView",
+            component: () => import("/@/views/index/user/push-view.vue")
           },
           {
-            path: 'messageView',
-            name: 'messageView',
-            component: () => import('/@/views/index/user/message-view.vue'),
-          },
-        ],
-      },
-    ],
+            path: "messageView",
+            name: "messageView",
+            component: () => import("/@/views/index/user/message-view.vue")
+          }
+        ]
+      }
+    ]
   },
   {
-    path: '/adminLogin',
-    name: 'adminLogin',
-    component: () => import('/@/views/admin/admin-login.vue'),
+    path: "/adminLogin",
+    name: "adminLogin",
+    component: () => import("/@/views/admin/admin-login.vue")
   },
   {
-    path: '/admin',
-    name: 'admin',
-    redirect: '/admin/order',
-    component: () => import('/@/views/admin/main.vue'),
+    path: "/admin",
+    name: "admin",
+    redirect: "/admin/order",
+    component: () => import("/@/views/admin/main.vue"),
     children: [
-      { path: 'overview', name: 'overview', component: () => import('/@/views/admin/overview.vue') },
-      { path: 'resource', name: 'resource', component: () => import('/@/views/admin/resource.vue') },
-      { path: 'order', name: 'order', component: () => import('/@/views/admin/order.vue') },
-      { path: 'thing', name: 'thing', component: () => import('/@/views/admin/thing.vue') },
-      { path: 'comment', name: 'comment', component: () => import('/@/views/admin/comment.vue') },
-      { path: 'user', name: 'user', component: () => import('/@/views/admin/user.vue') },
-      { path: 'classification', name: 'classification', component: () => import('/@/views/admin/classification.vue') },
-      { path: 'tag', name: 'tag', component: () => import('/@/views/admin/tag.vue') },
-      { path: 'notice', name: 'notice', component: () => import('/@/views/admin/notice.vue') },
-      { path: 'loginLog', name: 'loginLog', component: () => import('/@/views/admin/login-log.vue') },
-      { path: 'opLog', name: 'opLog', component: () => import('/@/views/admin/op-log.vue') },
-      { path: 'errorLog', name: 'errorLog', component: () => import('/@/views/admin/error-log.vue') },
-      { path: 'sysInfo', name: 'sysInfo', component: () => import('/@/views/admin/sys-info.vue') },
-    ],
-  },
+      {
+        path: "overview",
+        name: "overview",
+        component: () => import("/@/views/admin/overview.vue")
+      },
+      {
+        path: "resource",
+        name: "resource",
+        component: () => import("/@/views/admin/resource.vue")
+      },
+      {
+        path: "order",
+        name: "order",
+        component: () => import("/@/views/admin/order.vue")
+      },
+      {
+        path: "thing",
+        name: "thing",
+        component: () => import("/@/views/admin/thing.vue")
+      },
+      {
+        path: "comment",
+        name: "comment",
+        component: () => import("/@/views/admin/comment.vue")
+      },
+      {
+        path: "user",
+        name: "user",
+        component: () => import("/@/views/admin/user.vue")
+      },
+      {
+        path: "classification",
+        name: "classification",
+        component: () => import("/@/views/admin/classification.vue")
+      },
+      {
+        path: "tag",
+        name: "tag",
+        component: () => import("/@/views/admin/tag.vue")
+      },
+      {
+        path: "notice",
+        name: "notice",
+        component: () => import("/@/views/admin/notice.vue")
+      },
+      {
+        path: "loginLog",
+        name: "loginLog",
+        component: () => import("/@/views/admin/login-log.vue")
+      },
+      {
+        path: "opLog",
+        name: "opLog",
+        component: () => import("/@/views/admin/op-log.vue")
+      },
+      {
+        path: "errorLog",
+        name: "errorLog",
+        component: () => import("/@/views/admin/error-log.vue")
+      },
+      {
+        path: "sysInfo",
+        name: "sysInfo",
+        component: () => import("/@/views/admin/sys-info.vue")
+      }
+    ]
+  }
 ];
 
 export default constantRouterMap;
